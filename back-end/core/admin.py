6778cf0dd7068bd82f7ac98b89f24f33c4e5ac15
Togzhan Razuan <togzhanj07@gmail.com>
2026-04-19 23:01:50 +0500

    Add ModelAdmin classes for Task, DailyCheckIn, Film, and Book

--- back-end/core/admin.py
+++ back-end/core/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from .models import Task, DailyCheckIn, Film, Book
 
 admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'user', 'title', 'quadrant', 'is_done', 'created_at')
+    list_filter   = ('quadrant', 'is_done')
+    search_fields = ('title', 'user__username')
+  
 admin.site.register(DailyCheckIn)
+class DailyCheckInAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'user', 'date', 'score', 'sleep_hours', 'mood', 'energy_level')
+    list_filter   = ('date',)
+    search_fields = ('user__username',)
+    readonly_fields = ('score',)
+
 admin.site.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'user', 'title', 'director', 'genre', 'status', 'rating', 'added_at')
+    list_filter   = ('genre', 'status')
+    search_fields = ('title', 'director', 'user__username')
+
 admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'user', 'title', 'author', 'year', 'mood_tag', 'status', 'added_at')
+    list_filter   = ('mood_tag', 'status')
+    search_fields = ('title', 'author', 'user__username')
+
