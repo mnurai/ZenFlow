d75175120905b34cd0c68c2b2d6e5404bfd98c6d
mnurai <mukhambet.n.t@gmail.com>
2026-04-22 12:24:54 +0500

    Update serializers.py

--- back-end/core/serializers.py
+++ back-end/core/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
