356d577436f2226f1b58f7e0c743f55288b6f9fe
Togzhan Razuan <togzhanj07@gmail.com>
2026-04-22 10:49:44 +0500

    update 0004 film genre expand

--- back-end/core/migrations/0004_film_genre_expand.py
+++ back-end/core/migrations/0004_film_genre_expand.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0003_book_genre'),
+        ('core', '0002_add_fields_and_constraints'),
     ]
 
     operations = [
